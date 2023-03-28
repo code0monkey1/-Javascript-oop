ff8151797468737e809317057d932171f2e61068
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-03-28 10:58:16 +0000

    28 Mar 2023, 16:28

--- oop/cup.js
+++ oop/cup.js
@@ -1,8 +1,12 @@
 class Cup{
-
-  constructor(){
-
+   #name
+   #type
+  constructor(name,type){
+   this.#name=name;
+   this.#type=type;
   }
-
   
+   introduceYourself(){
+      console.log(`I am a cup of type ${this.#type} and you my name is ${this.#name}`);
+  }
 }
