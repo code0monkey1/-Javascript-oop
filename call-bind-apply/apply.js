438b445e9eb21bf0a475d1b6b007b5faee587b4a
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-03-29 09:10:12 +0000

    29 Mar 2023, 14:40

--- call-bind-apply/apply.js
+++ call-bind-apply/apply.js
@@ -1,10 +1,9 @@
-function add(...args) {
-   
+function add(one,two,three) {
+  
   const result = args.reduce((result,value)=> result+value)
   
   console.log("The result is: " + result)
 
 }
 
-
-add.apply(this,[1,2,3,4,5,6,7,8,9,10])
+add.apply(this,[1,2,3]);
